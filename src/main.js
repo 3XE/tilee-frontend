29ebd0db9ebd7cf970bacdb893a181463e8ed606
Peter Devoy <peter@3xe.co.uk>
2018-09-03 19:21:10 +0100

    rough in UI logic

--- src/main.js
+++ src/main.js
@@ -13,17 +13,50 @@ Tile.prototype.getAspectRatio = function () {
 };
 
 
+var Pattern = function (name, calculator) {
+    this.name = name;
+};
+
+var Surface = function (width, height) {
+    
+    this.width = ko.observable(width);
+    this.height = ko.observable(height);
+    this.area = ko.pureComputed(this.computeArea, this);
+};
+
+Surface.prototype.computeArea = function () {
+    console.log(this.width(), this.height());
+    return this.width() * this.height();
+};
+
 var vm = {};
 
-vm.tiles = ko.observableArray([]);
-vm.tiles.push(new Tile(6,3,"Subway",'"'));
-vm.tiles.push(new Tile(12,12,"Patio",'"'));
-vm.activeTile = ko.observable();
-vm.choosingTile = ko.observable(true);
+var subway = new Tile(6,3,"Subway",'"');
+var patio = new Tile(12,12,"Patio",'"');
+vm.tiles = ko.observableArray([subway, patio]);
+vm.activeTile = ko.observable(patio);
+vm.choosingTile = ko.observable(false);
+
+var diamond = new Pattern('Diamond');
+var rectangle = new Pattern('Rectangle');
+vm.patterns = ko.observableArray([diamond, rectangle]);
+vm.selectedPattern = ko.observable();
 
 vm.tileSelector = {
     selectedTile: ko.observable(),
 };
+
+vm.wall = new Surface(120, 120);
+vm.tileCount = ko.observable(0);
+
+var recalculate = function() {
+    //TODO: 
+    console.log('RECALCULATE...');
+};
+
+vm.wall.area.subscribe(recalculate);
+vm.selectedPattern.subscribe(recalculate);
+vm.activeTile.subscribe(recalculate);
 
 var controls = document.getElementById('controls');
 ko.applyBindings(vm, controls);
